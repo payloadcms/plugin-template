d197272644a0a92f0eb97c95b36f1a268d244a25
Jessica Boezwinkle <jessica@trbl.design>
2023-08-08 12:10:37 +0100

    fix: mock paths in jest config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   testTimeout: 60000,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/webpack/mocks/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/src/webpack/mocks/emptyModule.js',
+      '<rootDir>/src/mocks/mockFile.js',
+    '\\.(css|scss)$': '<rootDir>/src/mocks/mockFile.js',
   },
 }
